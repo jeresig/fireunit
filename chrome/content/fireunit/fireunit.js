acb7c4a5d586c6ad5ab480c40e25fdd985d58234
John Resig <jeresig@gmail.com>
2008-09-02 12:53:51 -0400

    Adding some event simulations to FireUnit and a basic test of the command line.

--- chrome/content/fireunit/fireunit.js
+++ chrome/content/fireunit/fireunit.js
@@ -27,7 +27,56 @@ Firebug.FireUnitModule = extend(Firebug.Module, {
                 panel.innerHTML += "<li><span style='color:" +
                     (pass ? "green" : "red") + ";'>" +
                     (pass ? "PASS" : "FAIL") + "</span> " + msg + "</li>";
-            }
+            },
+            click: function( node ){
+              if ( typeof node === "string" ) {
+                node = document.getElementById(node);
+              }
+
+              if ( node.click ) {
+                return node.click();
+              }
+
+              var doc = node.ownerDocument, event = doc.createEvent("MouseEvents");
+              event.initMouseEvent("click", true, true, doc.defaultView, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+              return node.dispatchEvent( event );
+            },
+            focus: function( node ){
+              if ( typeof node === "string" ) {
+                node = document.getElementById(node);
+              }
+
+              if ( node.focus ) {
+                return node.focus();
+              }
+
+              var doc = node.ownerDocument, event = doc.createEvent("UIEvents");
+              event.initUIEvent("DOMFocusIn", true, true, doc.defaultView, 1);
+              return node.dispatchEvent( event );
+            },
+            value: function( node, text ){
+              if ( typeof node === "string" ) {
+                node = document.getElementById(node);
+              }
+
+              node.value = text;
+            },
+            key: function( node, letter ){
+              if ( typeof node === "string" ) {
+                node = document.getElementById(node);
+              }
+
+              var keyCode = letter, charCode = 0;
+
+              if ( typeof keyCode == "string" ) {
+                charCode = keyCode.charCodeAt(0);
+                keyCode = 0;
+              }
+
+              var doc = node.ownerDocument, event = doc.createEvent("KeyEvents");
+              event.initKeyEvent("keypress", true, true, doc.defaultView, false, false, false, false, keyCode, charCode);
+              return node.dispatchEvent( event );
+            },
         };
     },
     
